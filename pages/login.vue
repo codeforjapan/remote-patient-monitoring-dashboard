888fc389f0e0f8d0bfd14de0742a994e774404d9
Kazuki Imamura <kaizumaki@gmail.com>
2020-12-30 21:30:34 +0900

    loginページを追加

---
+++ pages/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <PageHeader text="ログイン" />
+    <div class="inputContainer">
+      <InputField
+        label="ログインID"
+        :value="inputId"
+        @input="inputId = $event"
+      />
+      <InputField
+        label="パスワード"
+        type="password"
+        :value="inputPassword"
+        @input="inputPassword = $event"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PageHeader from '@/components/PageHeader.vue'
+import InputField from '@/components/InputField.vue'
+
+export default Vue.extend({
+  name: 'Index',
+  components: {
+    PageHeader,
+    InputField,
+  },
+  data() {
+    return {
+      inputId: '',
+      inputPassword: '',
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.inputContainer {
+  width: 100%;
+  max-width: 600px;
+}
+</style>
