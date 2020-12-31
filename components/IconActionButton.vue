7c77a4220c0bff2e9b9652cbbd54ff2f2f09b81a
Kazuki Imamura <kaizumaki@gmail.com>
2020-12-31 15:53:21 +0900

    患者データ一覧用のコンポーネントを追加

---
+++ components/IconActionButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class="iconActionButton" @click="$emit('click')">
+    <span>
+      <svg class="iconSvg" :is="icon" aria-hidden="true" />
+    </span>
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SortIcon from '@/static/icon-sort.svg'
+import FilterIcon from '@/static/icon-filter.svg'
+
+export default Vue.extend({
+  components: {
+    SortIcon,
+    FilterIcon,
+  },
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.iconActionButton {
+  border: none;
+  background: none;
+  font-size: 14px;
+}
+.iconSvg {
+  fill: $gray-3;
+}
+</style>
---
+++ components/SymptomsStatus.vue
@@ -0,0 +1,39 @@
+<template>
+  <span :class="['symptomsText', { active: isActive }]">
+    {{ text }}
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.symptomsText {
+  display: block;
+  font-size: 12px;
+  font-weight: bold;
+  color: $white;
+  text-align: center;
+  padding: 4px 8px;
+  margin: 6px;
+  border-radius: 3em;
+  background-color: $primary;
+  &.active {
+    background-color: $error;
+  }
+}
+</style>
