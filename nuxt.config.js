051c73946cad2fa1922389c01d57c64cd9f39aeb
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-01-06 15:03:20 +0900

    :wrench: metaにnoindexを設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { hid: 'robots', name: 'robots', content: 'noindex' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
