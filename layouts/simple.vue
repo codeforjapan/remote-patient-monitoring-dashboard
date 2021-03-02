d55fb6dc5157a41be48ab51d8f9788032a4645af
Kazuki Imamura <kaizumaki@gmail.com>
2021-03-02 13:41:14 +0900

    login用にlayoutを追加

---
+++ layouts/simple.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="mainContainer">
+    <div class="mainInner">
+      <Nuxt />
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.mainContainer {
+  background-color: $bg-gray;
+}
+.mainInner {
+  padding: 36px 30px 30px;
+}
+</style>
