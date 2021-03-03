769aefb6c20cf2517a888fd1c47796342c452827
Kazuki Imamura <kaizumaki@gmail.com>
2021-03-03 15:47:04 +0900

    [WIP]APIのurlにcenterIdを直書き・putメソッドを追加

--- services/PatientService.ts
+++ services/PatientService.ts
@@ -6,16 +6,44 @@ const API_URL = 'https://monitoring.stopcovid19.jp/stg/api/nurse/'
 
 class PatientService {
   async getPatients(): Promise<Patient[]> {
-    const response = await axios.get(API_URL + 'patients/', {
+    const response = await axios.get(
+      API_URL + 'centers/79167710-2761-4cf4-8888-241e9f0c36b8/patients/',
+      {
+        headers: authHeader(),
+      },
+    )
+    return response.data.Items
+  }
+
+  async postPatient(patient: ConsumePatient): Promise<Patient> {
+    const response = await axios.post(
+      API_URL + 'centers/79167710-2761-4cf4-8888-241e9f0c36b8/patients/',
+      patient,
+      {
+        headers: authHeader(),
+      },
+    )
+    return response.data
+  }
+
+  async getPatient(patientId: string): Promise<Patient> {
+    const response = await axios.get(API_URL + `patients/${patientId}`, {
       headers: authHeader(),
     })
     return response.data
   }
 
-  async postPatient(patient: ConsumePatient): Promise<Patient> {
-    const response = await axios.post(API_URL + 'patients/', patient, {
-      headers: authHeader(),
-    })
+  async putPatient(
+    patientId: string,
+    payload: ConsumePatient,
+  ): Promise<Patient> {
+    const response = await axios.put(
+      API_URL + `patients/${patientId}`,
+      payload,
+      {
+        headers: authHeader(),
+      },
+    )
     return response.data
   }
 }
