9c13c7622efdb305124d502ea982657d4f752941
Kazuki Imamura <kaizumaki@gmail.com>
2021-03-04 17:00:05 +0900

    新規患者登録モーダルを修正

--- components/PatientRegister.vue
+++ components/PatientRegister.vue
@@ -5,76 +5,78 @@
       入力された電話番号にショートメッセージにてログイン用のURLを送ります。<br />
       受信者に通信料金がかかる場合があります。
     </p>
-    <dl class="registrationList">
-      <dt class="registrationTitle">患者ID</dt>
-      <dd class="registrationItem">2022433050</dd>
-    </dl>
-
     <div class="inputContainer">
       <InputField
+        v-model="inputPatientId"
+        label="患者ID"
+        name="patientId"
+        required
+        rules="required"
+      />
+    </div>
+    <div class="inputContainer">
+      <InputField
+        v-model="inputMobileTel"
         label="携帯電話番号（ハイフン不要）"
         name="mobileTel"
-        :value="inputMobileTel"
         placeholder="例）09012345678"
-        @input="inputMobileTel = $event"
+        required
+        rules="required"
       />
     </div>
     <ActionButton
-      theme="primary"
+      type="submit"
+      :theme="btnTheme"
       size="L"
-      :is-inline="false"
-      @click="$emit('click-register')"
+      :is-submittable="isSubmittable"
+      @click="
+        $emit('click-register', {
+          patientId: inputPatientId,
+          mobileTel: inputMobileTel,
+        })
+      "
     >
       登録する
     </ActionButton>
+    <span class="error">{{ errorMessage }}</span>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 import InputField from '@/components/InputField.vue'
 import ActionButton from '@/components/ActionButton.vue'
 
-export default Vue.extend({
+@Component({
   components: {
     InputField,
     ActionButton,
   },
-  data() {
-    return {
-      inputMemo: '',
-      inputMobileTel: '',
-    }
-  },
 })
+export default class PatientRegister extends Vue {
+  inputPatientId = ''
+  inputMobileTel = ''
+
+  @Prop({ default: '' })
+  errorMessage: string
+
+  get isSubmittable(): boolean {
+    return this.inputPatientId !== '' && this.inputMobileTel !== ''
+  }
+
+  get btnTheme(): string {
+    return this.isSubmittable ? 'primary' : 'disable'
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.inputFieldContainer {
-  display: block;
-}
 .inputContainer {
-  width: 100%;
-  max-width: 600px;
   margin: 30px 0;
 }
-/deep/ .labelText {
+.error {
   display: block;
-  margin: 0 0 8px;
-  font-size: 16px;
-  font-weight: 400;
-}
-/deep/ .inputField {
-  width: 100%;
-}
-.registrationList {
-  margin: 40px 0 0;
-}
-.registrationTitle {
-  margin: 8px 0;
-}
-.registrationItem {
-  font-weight: 700;
-  margin-left: 0;
+  margin-top: 16px;
+  color: $error;
 }
 </style>
