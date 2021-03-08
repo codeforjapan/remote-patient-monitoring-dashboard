a7d50d256a1eee822fbb31cf3dc8d2a3708513ab
Kazuki Imamura <kaizumaki@gmail.com>
2021-03-08 16:25:40 +0900

    保健所選択のセレクトボックスをサイドバーに移動

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,29 @@
 <template>
   <div>
-    <div class="selectContainer">
-      <select v-model="selected" class="select" @change="handleChange">
-        <option value="">保健所を選択してください</option>
-        <option
-          v-for="(center, index) in centers"
-          :key="index"
-          :value="center.centerId"
-        >
-          {{ center.centerId }}
-        </option>
-      </select>
-    </div>
+    <p class="text">👈 保健所を選択してください</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { authStore, nursesStore } from '@/store'
-import { Center } from '@/types/component-interfaces/nurse'
 
 @Component
-export default class Index extends Vue {
-  centers: Center[] = []
-  selected = ''
-
-  created() {
-    if (authStore.user) {
-      nursesStore.load(authStore.user.username).then((nurse) => {
-        this.centers = nurse.manageCenters
-      })
-    } else {
-      authStore.signOut()
-      this.$router.push('/login')
-    }
-  }
-
-  handleChange() {
-    nursesStore.setCenter(this.selected)
-    this.$router.push(`/center/${this.selected}`)
-  }
-}
+export default class Index extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-.selectContainer {
-  margin-left: 16px;
-  position: relative;
-  border-radius: 10px;
-  background-color: $white;
-  &::before {
-    position: absolute;
-    top: 50%;
-    right: 0.8em;
-    margin-top: -2px;
-    width: 0;
-    height: 0;
-    padding: 0;
-    content: '';
-    border-left: 6px solid transparent;
-    border-right: 6px solid transparent;
-    border-top: 6px solid $gray-3;
-    pointer-events: none;
-  }
+.text {
+  font-size: 20px;
+  margin-top: 200px;
+  animation: arrow 0.7s alternate infinite;
 }
-.select {
-  width: 100%;
-  cursor: pointer;
-  text-overflow: ellipsis;
-  border: none;
-  outline: none;
-  background: transparent;
-  box-shadow: none;
-  appearance: none;
-  font-size: 16px;
-  padding: 12px 38px 12px 12px;
+
+@keyframes arrow {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(20px, 0);
+  }
 }
 </style>
