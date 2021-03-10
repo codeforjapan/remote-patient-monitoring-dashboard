d2c5839cc41bc46e746a4e630a0482ad4df3a9f5
Kazuki Imamura <kaizumaki@gmail.com>
2021-03-10 17:10:26 +0900

    refreshTokenのactionと有効期限切れのgetterを追加

--- store/modules/auth.ts
+++ store/modules/auth.ts
@@ -13,6 +13,16 @@ export default class AuthModule extends VuexModule {
   public status = storedUser ? { loggedIn: true } : { loggedIn: false }
   public user: AuthUser | null = storedUser ? JSON.parse(storedUser) : null
 
+  get isLoggedIn(): boolean {
+    return this.status.loggedIn
+  }
+
+  get isExpired(): boolean {
+    if (!this.user) return false
+    const payload: any = JSON.parse(atob(this.user!.idToken.split('.')[1]!))
+    return new Date().getSeconds() < payload.exp
+  }
+
   @Mutation
   public loginSuccess(user: AuthUser): void {
     this.status.loggedIn = true
@@ -54,13 +64,29 @@ export default class AuthModule extends VuexModule {
     )
   }
 
+  @Action({ rawError: true })
+  refreshToken(): Promise<AuthUser> {
+    return AuthService.refreshToken(this.user!.refreshToken).then(
+      (user) => {
+        this.context.commit('loginSuccess', user)
+        return Promise.resolve(user)
+      },
+      (error) => {
+        this.context.commit('loginFailure')
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.errorMessage) ||
+          error.message ||
+          error.toString()
+        return Promise.reject(message)
+      },
+    )
+  }
+
   @Action
   signOut(): void {
     AuthService.logout()
     this.context.commit('logout')
-  }
-
-  get isLoggedIn(): boolean {
-    return this.status.loggedIn
   }
 }
