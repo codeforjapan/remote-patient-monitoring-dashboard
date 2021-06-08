08c82ea5585a42b629ab8461d48c0a22d8444598
Kazuki Imamura <kaizumaki@gmail.com>
2021-06-08 18:48:06 +0900

    fix #260 centerディレクトリにindex.vueを追加

---
+++ pages/center/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { authStore } from '@/store'
+
+@Component
+export default class Index extends Vue {
+  created() {
+    if (authStore.user != null) {
+      authStore.refreshToken().then(() => {
+        this.$router.push('/')
+      })
+    }
+  }
+}
+</script>
